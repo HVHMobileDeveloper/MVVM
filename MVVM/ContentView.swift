cc14f0c458ab183dcb7f7c23293422425e5c5017
Huynh Van Hiep <hiephuynh200499@gmail.com>
2020-02-18 22:19:04 +0800

    Initial Commit

---
+++ MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVVM
+//
+//  Created by mobileteam on 2/18/20.
+//  Copyright © 2020 mobileteam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
